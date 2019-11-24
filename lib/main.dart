5dfead452153b32c4dd0e80afb4670756761c4a0
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2019-11-23 21:39:48 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.green,
+      primaryColor: Colors.white
+    ),
+  ));
+}
+
+Future<Map> getData() async {
+  String request = "https://api.hgbrasil.com/finance?format=json&key=5d33880b";
+  
+  http.Response response = await http.get(request);
+  print(json.decode(response.body));
+
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
+  void _realChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Conversor de moeda"),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch(snapshot.connectionState){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                    child: Text("Aguarde...",
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 30.0),
+                    textAlign: TextAlign.center,)
+                );
+              default:
+                if(snapshot.hasError){
+                  return Center(
+                      child: Text("Ops, houve uma falha ao buscar os dados",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 25.0),
+                        textAlign: TextAlign.center,)
+                  );
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.attach_money, size: 180.0, color: Colors.green),
+                        buildTextField("Reais", "R\$ ", realController, _realChanged),
+                        Divider(),
+                        buildTextField("Euros", "€ ", euroController, _euroChanged),
+                        Divider(),
+                        buildTextField("Dólares", "US\$ ", dolarController, _dolarChanged),
+                      ],
+                    ),
+                  );
+                }
+            }
+          })
+    );
+  }
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController c, Function f){
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.green),
+        border: OutlineInputBorder(),
+        prefixText: prefix
+    ),
+    style: TextStyle(
+        color: Colors.green, fontSize: 25.0
+    ),
+    onChanged: f,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+  );
+}
